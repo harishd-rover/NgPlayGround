29e450ab3aa03331c67cd503e81f7e926c069b8a
Harish D <harish.devendrappa@epicor.com>
2024-08-05 14:58:06 +0530

    changes

--- src/components/reactive-forms/reactive-forms.component.ts
+++ src/components/reactive-forms/reactive-forms.component.ts
@@ -1,31 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-forms.component.html',
-  styleUrl: './reactive-forms.component.css'
+  styleUrl: './reactive-forms.component.css',
 })
 export class ReactiveFormsComponent {
   userForm = new FormGroup({
     fname: new FormControl('', { validators: Validators.required }),
-    lname: new FormControl('', { validators: [Validators.required,  Validators.minLength(5)] }),
-    age: new FormControl('', { validators: [Validators.required, Validators.min(5), Validators.max(10)], })
-  })
+    lname: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        (control: AbstractControl) => {
+          if ((control.value as string).toLowerCase().includes('hari')) {
+            return null; // if validation successfull
+          } else {
+            return { PattenError: true }; // if not valid
+          }
+        },
+      ],
+    }),
+    age: new FormControl(5, {
+      validators: [Validators.required, Validators.min(5), Validators.max(10)],
+    }),
+  });
 
   onSubmit() {
-    console.log(this.userForm)
-    console.log(this.userForm.controls['fname'])
-    console.log(this.userForm.controls['lname'])
-    console.log(this.userForm.controls['age'])
+    console.log(this.userForm);
+    console.log(this.userForm.controls['fname']);
+    console.log(this.userForm.controls['lname']);
+    console.log(this.userForm.controls['age']);
 
-    console.log('first name', this.userForm.value.fname)
-    console.log('last name', this.userForm.value.lname)
+    console.log('first name', this.userForm.value.fname);
+    console.log('last name', this.userForm.value.lname);
 
     this.userForm.reset();
   }
-
 }
