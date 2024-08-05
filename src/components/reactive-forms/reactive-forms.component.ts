2ba5b7797d8973ad0d83286c316ac0bc71de09bc
Harish D <harish.devendrappa@epicor.com>
2024-08-05 13:01:35 +0530

    changes

--- src/components/reactive-forms/reactive-forms.component.ts
+++ src/components/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './reactive-forms.component.css'
 })
 export class ReactiveFormsComponent {
+  userForm = new FormGroup({
+    fname: new FormControl('', { validators: Validators.required }),
+    lname: new FormControl('', { validators: Validators.required }),
+    age: new FormControl('', { validators: [Validators.required, Validators.min(5), Validators.max(10)], })
+  })
+
+
+
+  onSubmit() {
+    console.log(this.userForm.controls['fname'])
+    console.log(this.userForm.controls['lname'])
+    console.log(this.userForm.controls['age'])
+
+    console.log('first name', this.userForm.controls.fname)
+    console.log('last name', this.userForm.controls.fname)
+
+  }
 
 }
